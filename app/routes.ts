651d55036055cce34936ab11c0dbe4dea2cbebb8
skyien-z <luckiskyi@gmail.com>
2025-01-01 19:53:32 -0600

    Finish arxiv publications script

--- app/routes.ts
+++ app/routes.ts
@@ -2,17 +2,15 @@ import {
     type RouteConfig,
     route,
     index,
-    layout,
-    prefix,
   } from "@react-router/dev/routes";
 
 export default [
-    route("/", "routes/scaffolding.tsx", [
-        index("routes/home.tsx"),
-        route("about", "routes/about.tsx"),
-        route("people", "routes/people.tsx"),
-        route("publications", "routes/publications.tsx"),
-        route("contact", "routes/contact.tsx"),
-      ]),
+  route("/", "routes/scaffolding.tsx", [
+      index("routes/home.tsx"),
+      route("about", "routes/about.tsx"),
+      route("people", "routes/people.tsx"),
+      route("publications", "routes/publications.tsx"),
+      route("contact", "routes/contact.tsx"),
+    ]),
   ] satisfies RouteConfig;
   
--- app/routes/publications.tsx
+++ app/routes/publications.tsx
@@ -1,13 +1,3 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Peopleee!" },
-  ];
-}
-
-export default function Home() {
-  return <><Welcome /><p>hi</p></>;
+export default function Publications() {
+  
 }
