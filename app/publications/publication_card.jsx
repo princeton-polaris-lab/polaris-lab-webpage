8f22bb258dc5f9f9ed37b1ef8cd3497794784034
skyien-z <luckiskyi@gmail.com>
2025-01-06 11:18:33 -0600

    Add basic search functionality to Publications

--- app/publications/publication_card.jsx
+++ app/publications/publication_card.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function PublicationCard(props) {
     return (
         <div className="card">
-            <h2>{props.title}</h2>
-            <p>{props.text}</p>
-            <p><strong>Authors:</strong> {props.authors.join(', ')}</p>
+            <h2>{props.publicationEntry.title}</h2>
+            <p>{props.publicationEntry.text}</p>
+            <p><strong>Authors:</strong> {props.publicationEntry.author.join(', ')}</p>
         </div>
     );
 };
--- app/routes/publications.jsx
+++ app/routes/publications.jsx
@@ -1,16 +1,39 @@
-import publications from "../publications/publications.json"
+import publicationsJson from "../publications/publications.json"
 import PublicationCard from "../publications/publication_card.jsx"
 
+import React, { useEffect, useState } from 'react';
+
 export default function Publications() {
+    const [publications, setPublications] = useState(publicationsJson)
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    useEffect(() => {
+        const filteredPublications = []
+        for (const publication of publicationsJson) {
+            if (publication.title.includes(searchTerm) || 
+                publication.abstract.includes(searchTerm)) {
+                filteredPublications.push(publication)
+            }
+        }
+        setPublications(filteredPublications)
+    }, [searchTerm]);
 
     return (
         <div className="page">
-            { publications.map((publication, index) => (
+            <input 
+                type="text" 
+                placeholder="Search..." 
+                value={searchTerm} 
+                onChange={handleInputChange} 
+            />
+            { publications.map((publicationEntry, index) => (
                 <PublicationCard
                     key={index}
-                    title={publication.title}
-                    text={publication.pdf}
-                    authors={publication.author}
+                    publicationEntry={publicationEntry}
                 />
             ))}
         </div>
