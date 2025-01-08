5dd9df8a6e3a6c9bc587f3b432416f292760f7b7
skyien-z <luckiskyi@gmail.com>
2025-01-07 21:56:34 -0600

    Add People Page

--- app/people/profile_card.jsx
+++ app/people/profile_card.jsx
@@ -1,10 +1,23 @@
+import { Popup } from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const filePathToProfileImages = "/profile_pictures/"
+
+function ProfilePopup( {profile} ) {
+    return (
+        <Popup trigger={<button> Trigger</button>} position="right center" modal>
+            <div>Popup content here !!</div>
+        </Popup>
+    )
+}
+
 export default function ProfileCard( {profile} ) {
     return (
         <div>
-            <img src={filePathToProfileImages + profile.file} />
+            <img width={250} height={250} alt={"Image of " + profile.name} 
+                src={filePathToProfileImages + profile.file} />
             <p>{profile.name}</p>
+            <ProfilePopup profile={profile}/>
         </div>
     )
 }
