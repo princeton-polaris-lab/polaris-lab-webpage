c35fca8100c216588f54deedd0c7b5c75b69ae02
skyien-z <luckiskyi@gmail.com>
2025-01-20 15:53:01 -0500

    Add publications to homepage

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,4 +1,4 @@
-import pressArticles from "../press/press.json"
+import pressArticles from "./press/press.json"
 
 const robot_filepath = "./robot_images/"
 export default function Home() {
@@ -9,6 +9,40 @@ export default function Home() {
         alt={"Cute robot looking to the left against a white backdrop"}
       />
       <h1>Hi! This site is under construction. 🦖 </h1>
+      <h2>Press</h2>
+      <PressArticles/>
     </div>
   );
+}
+
+function PressArticles() {
+  return (
+    <div className="press-div">
+      {
+        pressArticles.map((article, index) => (
+          <div>
+            <ArticleCard article={article}/>
+          </div>
+        )
+      )
+      }
+    </div>
+  )
+}
+
+function ArticleCard( {article} ) {
+  return (
+    <div className="article-card">
+      <h2> 
+        <a href={article.original_url} target="_blank" rel="noreferrer">
+          {article.title}
+        </a>
+      </h2>
+      <img 
+        width={"auto"}
+        height={250}
+        src={require("./press/" + article.top_image_relative_path)}
+        alt={"Top image from press article " + article.title}/>
+    </div>
+  )
 }
